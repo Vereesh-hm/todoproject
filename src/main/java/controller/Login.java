ff8b82ad4e10441ae55c22acc3e913e47573b936
vereesh hm <hmvereesh@gmail.com>
2023-11-30 17:47:40 +0530

    updated

---
+++ src/main/java/controller/Login.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.ant.jmx.JMXAccessorGetTask;
+
+import dao.UserDao;
+import dto.UserDto;
+import service.UserService;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+import dto.UserDto;
+import service.UserService;
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+
+		UserService service = new UserService();
+		if (service.login(email, password)) {
+			UserDao dao=new UserDao();
+			UserDto dto=dao.findByEmail(email);
+			req.getSession().setAttribute("user",dao.findByEmail(email));
+			resp.getWriter().print("<h1 align='center' style='color:green'>Login Success</h1>");
+			req.setAttribute("list",dto.getTasks());
+			req.getRequestDispatcher("Home.jsp").include(req, resp);
+		} else {
+			resp.getWriter().print("<h1 align='center' style='color:red'>Invalid Credentials</h1>");
+			req.getRequestDispatcher("Login.html").include(req, resp);
+		}
+	}
+}
---
+++ src/main/webapp/Home.jsp
@@ -0,0 +1,53 @@
+<%@page import="dto.Task"%>
+<%@page import="java.util.List"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+</style>
+<meta charset="ISO-8859-1">
+<title>Todo Home</title>
+</head>
+<body>   
+     <%
+	List<Task> list = (List<Task>) request.getAttribute("list");
+	%>
+	<div>
+		<h1>Todo Home</h1>
+		<table border="1px solid black">
+			<tr>
+				<th>Task Name</th>
+				<th>Task Description</th>
+				<th>Date Created</th>
+				<th>Status</th>
+				<th>Delete</th>
+				<th>Edit</th>
+			</tr>
+			<%
+			   if(list!=null)
+			   {
+			    for(Task task:list){%>
+			<tr>
+				<th><%=task.getName()%></th>
+				<th><%=task.getDescription()%></th>
+				<th><%=task.getCreatedTime()%></th>
+				<th><%=task.isStatus()%></th>
+				<td><button>Delete</button></td>
+				<td><button>Edit</button></td>
+			</tr>
+			<%} }%>
+		</table>
+		<br>
+		<a href="session-add-task"><button>Add Task</button></a><br>
+		<a href="logout"><button>Logout</button></a>
+	</div>
+</body>
+</html>
